ee5ebe2871732be38203c330ce159745b574d87d
dsiah <dsiah@villanova.edu>
2020-02-19 01:20:41 -0800

    initial commit

---
+++ src/main/java/libsiah/LinkedList.java
@@ -0,0 +1,37 @@
+package libsiah;
+
+public class LinkedList<T> {
+    private T val;
+    private LinkedList<T> next;
+
+    public LinkedList(T val, LinkedList<T> next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public LinkedList(T val) {
+        this.val = val;
+    }
+
+    public LinkedList() {}
+
+    public T getVal() {
+        return val;
+    }
+
+    public LinkedList<T> getNext() {
+        return next;
+    }
+
+    public boolean hasNext() {
+        return next == null;
+    }
+
+    public void setVal(T val) {
+        this.val = val;
+    }
+
+    public void setNext(LinkedList<T> next) {
+        this.next = next;
+    }
+}
